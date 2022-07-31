44416b6656e3ea02628fa147b10e9e3627b48082
yanglin <yanglin556@yahoo.com>
2022-07-31 21:40:15 +0800

    init

---
+++ vue-demo/src/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="red">
+    {{n}}
+    <button @click="app">+1</button>
+</div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            n:0
+        }
+    },
+    methods:{
+        add(){
+            this.n += 1
+        }
+    }
+};
+</script>
+
+<style scoped>
+   .red{
+    color:red;
+}
+</style>
