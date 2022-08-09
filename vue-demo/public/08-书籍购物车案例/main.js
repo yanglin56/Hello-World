72b930125993e0d96b90d3ace515dfbf66224153
yanglin <yanglin556@yahoo.com>
2022-08-09 23:05:23 +0800

    init

--- vue-demo/public/08-书籍购物车案例/main.js
+++ vue-demo/public/08-书籍购物车案例/main.js
@@ -47,13 +47,31 @@ const app = new Vue({
     },
     computed: {
         totalPrice() {
-            let totalPrice = 0;
-            for (let i = 0; i < this.books.length; i++) {
-                totalPrice += this.books[i].price * this.books[i].count
-            }
-            return totalPrice
-                // for(let i in/of this.books)
-                // reduce
+            // 1.普通的for循环
+            // let totalPrice = 0;
+            // for (let i = 0; i < this.books.length; i++) {
+            // totalPrice += this.books[i].price * this.books[i].count
+            // }
+            // return totalPrice
+
+            // 2.for (let i in this.books)
+            // let totalPrice = 0;
+            // for (let i in this.books) {
+            //     const books=this.books[i]
+            //     totalPrice += books.price * books.count
+            // }
+            // return totalPrice
+
+            // 3.for(let i of this.books)
+            // let totalPrice = 0;
+            // for (let item of this.books) {
+            //     totalPrice += item.price * item.count
+            // }
+            // return totalPrice
+
+            return this.books.reduce(function(preValue, book) {
+                return preValue + book.price * book.count
+            }, 0)
         }
     },
     filters: { //过滤器
@@ -62,3 +80,67 @@ const app = new Vue({
         }
     }
 })
+
+// 编程范式：命令式编程/声明式编程
+// 编程范式：面向对象编程(第一公民：对象)/函数式编程(第一公民：函数)
+// filter/map/reduce
+// filter中的回调函数有一个要求，必须返回一个boolean值
+// true:当返回true时，函数内部会自动将这次回调的n加入到新的数组中
+// false:当返回false时，函数内部会过滤这次的n
+const nums = [10, 20, 30, 50, 100, 111, 223, 323, 40, 50]
+
+// let total = nums.filter(n => n < 100).map(n => n * 2).reduce((pre, n) => pre + n);
+// console.log(total);
+
+let total = nums.filter(function(n) {
+    return n < 100
+}).map(function(n) {
+    return n * 2
+}).reduce(function(preValue, n) {
+    return preValue + n
+}, 0)
+console.log(total)
+
+// 1.filter函数的使用
+// 10，20，30，50 40 50
+// let newNums = nums.filter(function(n) {
+//         return n < 100
+//     })
+// console.log(newNums);
+
+// 2.map函数的使用
+// let new2Nums = newNums.map(function(n) {
+//     return n * 2
+// })
+// console.log(new2Nums);
+
+// 3.reduce函数的使用
+// reduce作用对数组中所有的内容进行汇总
+// let total = new2Nums.reduce(function(preValue, n) {
+//     return preValue + n
+// }, 0)
+
+// 第一次：preValue 0 n 10
+// 第二次：preValue 10 n 20
+// 第三次：preValue 30 n 30
+// 第四次：preValue 60 n 50
+
+// // 1.需求:取出所有小于100的数字
+// let newNums = []
+// for (let n of nums) {
+//     if (n < 100) {
+//         newNums.push(n)
+//     }
+// }
+// // 2.需求:将所有小于100的数字进行转化：全部*2
+// let new2Nums = []
+// for (let n of newNums) {
+//     new2Nums.push(n * 2)
+// }
+// console.log(new2Nums);
+// // 3.需求:将所有new2Nums数字相加，得到最终的结果
+// let total = 0;
+// for (let n of new2Nums) {
+//     total += n
+// }
+// console.log(total);
