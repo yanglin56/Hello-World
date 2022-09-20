7db7b3d40fb855f11c76f5a562430193ff9947f2
yanglin <yanglin556@yahoo.com>
2022-09-20 23:58:01 +0800

    创建tabbar

---
+++ 06 814/LearnVuejs07/02-tabbar/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+import TabBar from "./components/tabbar/TabBar";
+export default {
+  name: "App",
+  components: {
+    TabBar,
+  },
+};
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
---
+++ 06 814/LearnVuejs07/02-tabbar/src/components/tabbar/TabBar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="tab-bar">
+    <div class="tar-bar-item">首页</div>
+    <div class="tar-bar-item">分类</div>
+    <div class="tar-bar-item">购物车</div>
+    <div class="tar-bar-item">我的</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LearnVuejs07TabBar",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style  scoped>
+#tab-bar {
+  display: flex;
+  background: #f6f6f6;
+
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  box-shadow: 0 -1px 1px rgba(100, 100, 100, 0.2);
+}
+.tar-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+}
+</style>
