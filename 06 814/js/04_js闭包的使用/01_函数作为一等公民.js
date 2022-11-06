fc52dcc62c6a903d346cdf4c979de85bea162c86
yanglin <yanglin556@yahoo.com>
2022-11-06 21:38:47 +0800

    js闭包的使用

---
+++ 06 814/js/04_js闭包的使用/01_函数作为一等公民.js
@@ -0,0 +1,38 @@
+// function foo(org) {
+//   console.log("foo", org)
+// }
+
+// foo(123)
+
+// 将函数作为另外一个函数的参数
+// function foo(aaaa) {
+//   aaaa()
+// }
+
+// function bar() {
+//   console.log("bar")
+// }
+
+// foo(bar)
+
+// 案例
+function calc(num1, num2, calcFn) {
+  console.log(calcFn(num1, num2))
+}
+
+function add(num1, num2) {
+  return num1 + num2
+}
+
+function sub(num1, num2) {
+  return num1 - num2
+}
+
+function mul(num1, num2) {
+  return num1 * num2
+}
+
+var m = 30
+var n = 20
+
+calc(m, n, mul)
---
+++ 06 814/js/04_js闭包的使用/03_数组中的函数使用.js
@@ -0,0 +1,11 @@
+var nums = [10, 5, 11, 100, 55]
+
+// var newNums = []
+// for (var i = 0; i < nums.length; i++) {
+//   var num = nums[i]
+//   if (num % 2 === 0) {
+//     newNums.push(num)
+//   }
+// }
+
+// console.log(newNums)
