f6ea6746e505e74d9187f45f7b8a3c3746371fe2
yanglin <yanglin556@yahoo.com>
2022-12-13 21:59:51 +0800

    函数的原型理解

--- 06 814/js/12_JS创建对象方案/04_创建对象方案-构造函数.js
+++ 06 814/js/12_JS创建对象方案/04_创建对象方案-构造函数.js
@@ -14,8 +14,11 @@ function Person(name, age, height, address) {
   }
 }
 
-var p1 = new person("张三", 18, 1.88, "上海市")
-var p2 = new person("李四", 20, 1.78, "北京市")
+var p1 = new Person("张三", 18, 1.88, "上海市")
+var p2 = new Person("李四", 20, 1.78, "北京市")
 
 console.log(p1)
 console.log(p2)
+
+console.log(p1.eating === p2.eating)
+console.log(p1.running === p2.running)
