2fe01863f7d641feb5c7905c2e60ef386a0c6121
yanglin <yanglin556@yahoo.com>
2022-12-15 21:48:00 +0800

    原型链的理解

---
+++ 06 814/js/13_JS原型链和继承/02_原型链的理解.js
@@ -0,0 +1,19 @@
+var obj = {
+  name: "why",
+  age: 18
+}
+
+// [[get]]操作
+// 1.在当前的对象中查找属性
+// 2.如果没有找到，这个时候会去原型(__proto__)对象上查找
+
+obj.__proto__ = {
+
+}
+
+// 原型链
+obj.__proto__.__proto__ = {
+  address: "广州市"
+}
+
+console.log(obj.address)
