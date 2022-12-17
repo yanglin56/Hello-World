582dab0c3bcb844fdfdc6c998b5104016cef3f72
yanglin <yanglin556@yahoo.com>
2022-12-17 23:58:16 +0800

    顶层原型

---
+++ 06 814/js/13_JS原型链和继承/04_顶层原型来自哪里.js
@@ -0,0 +1,28 @@
+// var obj1 = {} // 创建了一个对象
+// var obj2 = new Object() // 创建了一个对象
+
+// function Person() {
+
+// }
+
+// var p = new Person()
+
+var obj = {
+  name: "why",
+  age: 18
+}
+
+obj.prototype = {
+  address: "北京市"
+}
+obj.__proto__ = obj2
+
+// Object.prototype
+// console.log(obj.__proto__)
+// console.log(Object.prototype)
+// console.log(obj.__proto__ === Object.prototype)
+
+console.log(Object.prototype)
+console.log(Object.prototype.constructor)
+
+console.log(Object.getOwnPropertyDescriptors(Object.prototype))
