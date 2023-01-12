632320cd14ba755a5a700576faf7a067a217431d
yanglin <yanglin556@yahoo.com>
2023-01-12 23:57:31 +0800

    _instanceof的判断

---
+++ 06 814/js/14_JS原型内容补充/02_instanceof的判断.js
@@ -0,0 +1,30 @@
+function createObject(o) {
+  function Fn() { }
+  Fn.prototype = o
+  return new Fn()
+}
+
+function inheritPrototype(SubType, SuperType) {
+  SubType.prototype = Object.create(SuperType.prototype)
+  Object.defineProperty(SubType.prototype, "constructor", {
+    enumerable: false,
+    configurable: true,
+    writable: true,
+    value: SubType
+  })
+}
+
+
+function Person() {
+
+}
+
+function Student() {
+
+}
+
+inheritPrototype(Student, Person)
+
+var stu = new Student()
+console.log(stu instanceof Student) // true
+console.log(stu instanceof Person) // true/false
